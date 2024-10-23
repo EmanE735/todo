2be188629bcaa645acc692e1b4ec216e9d3f5434
eman emad <emanemad0055@gmail.com>
2024-10-23 10:15:50 +0300

    finish home ui

---
+++ lib/app_theme.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class AppTheme{
+  static const Color lightprimary = Color(0xFFDFECDB);
+   static const Color darkprimary = Color(0xFF200E32);
+    static const Color primary = Color(0xFF3598DB);
+     static const Color black = Color(0xFF060E1E);
+     static const Color white = Color(0xFFFFFFFF);
+      static const Color green = Color(0xFF61E757);
+      static const Color red = Color(0xFFEC4B4B);
+      static const Color grey = Color(0xFFC8C9CB);
+  static ThemeData lighttheme = ThemeData(
+    scaffoldBackgroundColor: lightprimary,
+    primaryColor: primary,
+   bottomNavigationBarTheme:BottomNavigationBarThemeData(
+    type: BottomNavigationBarType.fixed,
+    backgroundColor: white,
+    selectedItemColor: primary,
+    unselectedItemColor: grey,
+   ),
+   floatingActionButtonTheme: FloatingActionButtonThemeData(
+    backgroundColor: primary,
+    foregroundColor: white,
+    shape: CircleBorder(
+      side: BorderSide(
+        width: 4,
+        color: white
+      )
+    )
+   )
+  
+  );
+}
---
+++ lib/home_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:todo/app_theme.dart';
+import 'package:todo/tabs/settings/settings_tab.dart';
+import 'package:todo/tabs/tasks/tasks_tab.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  static const String routeName= "/home";
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  List <Widget> tabs =[
+    SettingsTab(),
+    TasksTab(),
+
+  ];
+  int currentTabIndex =0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: tabs[currentTabIndex],
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        color: AppTheme.white,
+        padding: EdgeInsets.zero,
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        notchMargin: 10,
+        child: BottomNavigationBar(
+          currentIndex:  currentTabIndex,
+          onTap: (index)=> setState(() {
+            currentTabIndex=index;
+          }),
+          items: [
+            BottomNavigationBarItem(
+              label:"tasks" ,
+              icon: Icon(Icons.list,
+            
+            )),
+             BottomNavigationBarItem(
+              label:"settings" ,
+              icon: Icon(Icons.settings)),
+            ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton
+      (onPressed: (){},
+      child: Icon(Icons.add,
+      size: 32,
+      
+      ),
+      
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+   
+
+    );
+  }
+}
